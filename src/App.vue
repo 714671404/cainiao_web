1fd3dd40669093eac40d570950280c73cb635723
714671404 <714671404@qq.com>
2019-09-24 22:04:56 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
     <div id="app">
         <router-link to="/">home</router-link>
         <router-link to="/about">about</router-link>
+        <a href="/test" target="_blank">跳转其他页面</a>
         <router-view></router-view>
     </div>
 </template>
